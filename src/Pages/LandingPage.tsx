b383154ae708c838b96e782538e84bc750bde3b7
ArmyaAli <ali.umar.work@gmail.com>
2021-02-07 12:57:51 -0500

    updated landing page structure

--- src/Pages/LandingPage.tsx
+++ src/Pages/LandingPage.tsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React from "react";
+import { SwipeableDrawer } from "@material-ui/core";
+import NavBar from "../Components/NavBar";
 
 export const LandingPage = () => {
-    return (
-        <div>
-            Hello World
-        </div>
-    );
-}
+  const [drawerState, setdrawerState] = React.useState<boolean>(false);
+
+  const toggleDrawer = ($event: React.SyntheticEvent<{}, Event>) => {
+    setdrawerState(false);
+  };
+
+  return (
+    <div>
+      <NavBar drawrStateSetter={setdrawerState} />
+      <SwipeableDrawer
+        style={{ width: "25%" }}
+        anchor={"left"}
+        open={drawerState}
+        onClose={toggleDrawer}
+        onOpen={toggleDrawer}
+      >
+          
+      </SwipeableDrawer>
+    </div>
+  );
+};
