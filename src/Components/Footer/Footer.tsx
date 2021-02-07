ef997854b3b8c0a0619c074ff8642a494b746e2e
ArmyaAli <ali.umar.work@gmail.com>
2021-02-07 17:38:10 -0500

    restructured components folder to  MVC likepattern..

---
+++ src/Components/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Footer = () => {
+    return (
+        <div style={{backgroundColor: 'lightblue', height: '256px', width: '100%'}}>
+            
+        </div>
+    )
+}
