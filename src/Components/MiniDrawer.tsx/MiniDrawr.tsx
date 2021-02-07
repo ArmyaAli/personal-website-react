6fa9a7b1f96dd54b59af2ba0f097b43e0bb61e3f
ArmyaAli <ali.umar.work@gmail.com>
2021-02-07 17:38:29 -0500

    miniDrawer trempalting from mat ui

---
+++ src/Components/MiniDrawer.tsx/MiniDrawr.tsx
@@ -0,0 +1,123 @@
+import React from "react";
+import clsx from "clsx";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import Drawer from "@material-ui/core/Drawer";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import List from "@material-ui/core/List";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import HomeRoundedIcon from '@material-ui/icons/HomeRounded';
+import InfoRoundedIcon from '@material-ui/icons/InfoRounded';
+import WorkRoundedIcon from '@material-ui/icons/WorkRounded';
+import ReceiptRoundedIcon from '@material-ui/icons/ReceiptRounded';
+import CreateRoundedIcon from '@material-ui/icons/CreateRounded';
+import ContactMailRoundedIcon from '@material-ui/icons/ContactMailRounded';
+import { useDrawerAppBarStypes } from "./miniDrawerStyles";
+
+const iconMap = {
+    "Home": <HomeRoundedIcon />,
+    "About Me": <InfoRoundedIcon />,
+    "Projects": <WorkRoundedIcon />,
+    "Resume": <ReceiptRoundedIcon />,
+    "Technical Skills": <CreateRoundedIcon/>,
+    "Contact": <ContactMailRoundedIcon/>
+}
+
+const ROUTES = [
+  "Home",
+  "About Me",
+  "Projects",
+  "Resume",
+  "Technical Skills",
+  "Contact",
+];
+
+export default function MiniDrawer() {
+  const classes = useDrawerAppBarStypes();
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        className={clsx(classes.appBar, {
+          [classes.appBarShift]: open,
+        })}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            className={clsx(classes.menuButton, {
+              backgroundColor: true,
+              [classes.hide]: open,
+            })}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap>
+            Ali Umar - Jr Developer
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        variant="permanent"
+        className={clsx(classes.drawer, {
+          [classes.drawerOpen]: open,
+          [classes.drawerClose]: !open,
+        })}
+        classes={{
+          paper: clsx({
+            [classes.drawerOpen]: open,
+            [classes.drawerClose]: !open,
+          }),
+        }}
+      >
+        <div className={classes.toolbar}>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === "rtl" ? (
+              <ChevronRightIcon />
+            ) : (
+              <ChevronLeftIcon />
+            )}
+          </IconButton>
+        </div>
+        <Divider />
+        <List>
+          {ROUTES.map((text: string) => {
+
+            return (
+              <ListItem button key={text}>
+                <ListItemIcon>
+                    {iconMap[text]}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItem>
+            );
+          })}
+        </List>
+      </Drawer>
+    </div>
+  );
+}
