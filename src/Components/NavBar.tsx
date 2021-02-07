a1aac204055c5372328495e0abcf9a0cb46bd3ee
ArmyaAli <ali.umar.work@gmail.com>
2021-02-07 12:57:40 -0500

    updated navbar base with styling

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -1,40 +1,59 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
 
 const HEADING = "Ali Umar - Jr Developer";
 
+interface NavBarProps {
+    drawrStateSetter: React.Dispatch<React.SetStateAction<boolean>>
+}
+
 const useStyles = makeStyles((theme) => ({
-    root: {
-      flexGrow: 1,
-    },
-    menuButton: {
-      marginRight: theme.spacing(2),
-    },
-    title: {
-      flexGrow: 1,
-    },
-  }));
-  
-  export default function NavBar() {
-    const classes = useStyles();
-  
-    return (
-      <div className={classes.root}>
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" className={classes.title}>
-                {HEADING}
-            </Typography>
-          </Toolbar>
-        </AppBar>
-      </div>
-    );
+  root: {
+    flexGrow: 1,
+    backgroundColor: "#4f3a4d",
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+export default function NavBar(props: NavBarProps) {
+  const classes = useStyles();
+
+  const handleMenuButtonClick = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    props.drawrStateSetter(true);
   }
+  return (
+    <div className={classes.root}>
+      <AppBar
+        position="fixed"
+        classes={{
+          colorPrimary: classes.root, // class name, e.g. `classes-nesting-root-x`
+        }}
+      >
+        <Toolbar>
+          <IconButton 
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+            onClick={handleMenuButtonClick}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            {HEADING}
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
