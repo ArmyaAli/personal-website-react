0ad8de75e99794948b4a69e7af82808903ed99f4
ArmyaAli <ali.umar.work@gmail.com>
2021-02-07 17:38:39 -0500

    added particlejs react component

---
+++ src/Components/ParticlesBackground/ParticlesBackground.tsx
@@ -0,0 +1,91 @@
+import Particles from "react-tsparticles";
+import React from "react";
+
+export const ParticlesBackground = () => {
+  return (
+    <div >
+      <Particles
+        style={{position: 'fixed', width: '100%', height: '100%', zIndex: -2, top: 0, bottom: 0, right: 0, left:0}}
+        id="tsparticles"
+        options={{
+          background: {
+            color: {
+              value: "#0d47a1",
+            },
+          },
+          fpsLimit: 60,
+          interactivity: {
+            detectsOn: "canvas",
+            events: {
+              onClick: {
+                enable: true,
+                mode: "push",
+              },
+              onHover: {
+                enable: true,
+                mode: "repulse",
+              },
+              resize: true,
+            },
+            modes: {
+              bubble: {
+                distance: 400,
+                duration: 2,
+                opacity: 0.8,
+                size: 40,
+              },
+              push: {
+                quantity: 4,
+              },
+              repulse: {
+                distance: 200,
+                duration: 0.4,
+              },
+            },
+          },
+          particles: {
+            color: {
+              value: "#ffffff",
+            },
+            links: {
+              color: "#ffffff",
+              distance: 150,
+              enable: true,
+              opacity: 0.5,
+              width: 1,
+            },
+            collisions: {
+              enable: true,
+            },
+            move: {
+              direction: "none",
+              enable: true,
+              outMode: "bounce",
+              random: false,
+              speed: 6,
+              straight: false,
+            },
+            number: {
+              density: {
+                enable: true,
+                value_area: 800,
+              },
+              value: 80,
+            },
+            opacity: {
+              value: 0.5,
+            },
+            shape: {
+              type: "circle",
+            },
+            size: {
+              random: true,
+              value: 5,
+            },
+          },
+          detectRetina: true,
+        }}
+      />
+    </div>
+  );
+};
