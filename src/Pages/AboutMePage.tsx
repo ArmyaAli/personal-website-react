ae510858b5c61dd64a08aa37b165566531665e6c
ArmyaAli <ali.umar.work@gmail.com>
2021-02-07 23:23:31 -0500

    created about me page template

---
+++ src/Pages/AboutMePage.tsx
@@ -0,0 +1,12 @@
+import { Header } from "../Components/Header/Header";
+import { Footer } from "../Components/Footer/Footer";
+import MiniDrawr from "../Components/MiniDrawer.tsx/MiniDrawr";
+import { ParticlesBackground } from "../Components/ParticlesBackground/ParticlesBackground";
+
+export const AboutMePage = () => {
+  return (
+    <div>
+      <div>About Me Page</div>
+    </div>
+  );
+};
