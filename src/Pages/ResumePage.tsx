2ddd244cdf4c49e2f5f16ebc09cfb0ac29cc43e2
ArmyaAli <ali.umar.work@gmail.com>
2021-02-07 23:24:27 -0500

    created resume page template

---
+++ src/Pages/ResumePage.tsx
@@ -0,0 +1,15 @@
+import { Header } from "../Components/Header/Header";
+import { Footer } from "../Components/Footer/Footer";
+import MiniDrawr from "../Components/MiniDrawer.tsx/MiniDrawr";
+import { ParticlesBackground } from "../Components/ParticlesBackground/ParticlesBackground";
+
+export const ResumePage = () => {
+    return (
+      <div>
+        <div style={{ width: '100%', height: '512px', backgroundColor: 'green'}}>
+            Resume Page
+        </div>
+      </div>
+    );
+  };
+  
