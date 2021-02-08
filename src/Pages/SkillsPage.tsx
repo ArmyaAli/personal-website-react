9d8bd5bc8ce71c4d6b3a7d7621a3c5877243f04f
ArmyaAli <ali.umar.work@gmail.com>
2021-02-07 23:24:39 -0500

    created skills page template

---
+++ src/Pages/SkillsPage.tsx
@@ -0,0 +1,15 @@
+import { Header } from "../Components/Header/Header";
+import { Footer } from "../Components/Footer/Footer";
+import MiniDrawr from "../Components/MiniDrawer.tsx/MiniDrawr";
+import { ParticlesBackground } from "../Components/ParticlesBackground/ParticlesBackground";
+
+export const SkillsPage = () => {
+    return (
+      <div>
+        <div style={{ width: '100%', height: '512px', backgroundColor: 'green'}}>
+            Skills Page
+        </div>
+      </div>
+    );
+  };
+  
