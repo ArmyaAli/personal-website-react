58313f08b6c1710635cddf7ea968d1df82d5d7dc
ArmyaAli <ali.umar.work@gmail.com>
2021-02-07 23:23:45 -0500

    created contact page template

---
+++ src/Pages/ContactPage.tsx
@@ -0,0 +1,13 @@
+import { Header } from "../Components/Header/Header";
+import { Footer } from "../Components/Footer/Footer";
+import MiniDrawr from "../Components/MiniDrawer.tsx/MiniDrawr";
+import { makeStyles } from "@material-ui/core/styles";
+import { ParticlesBackground } from "../Components/ParticlesBackground/ParticlesBackground";
+
+export const ContactPage = () => {
+  return (
+    <div>
+      <div>Contact Page</div>
+    </div>
+  );
+};
