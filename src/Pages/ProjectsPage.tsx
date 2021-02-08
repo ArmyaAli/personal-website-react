945972187ea29233919e2ac921189e553595693d
ArmyaAli <ali.umar.work@gmail.com>
2021-02-07 23:24:16 -0500

    project page template

---
+++ src/Pages/ProjectsPage.tsx
@@ -0,0 +1,15 @@
+import { Header } from "../Components/Header/Header";
+import { Footer } from "../Components/Footer/Footer";
+import MiniDrawr from "../Components/MiniDrawer.tsx/MiniDrawr";
+import { ParticlesBackground } from "../Components/ParticlesBackground/ParticlesBackground";
+
+export const ProjectsPage = () => {
+    return (
+      <div>
+        <div style={{ width: '100%', height: '512px', backgroundColor: 'green'}}>
+          Projects Page            
+        </div>
+      </div>
+    );
+  };
+  
