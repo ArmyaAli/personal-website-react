b6862ca25fb17c0b7248069ba270e415072b34f3
ArmyaAli <ali.umar.work@gmail.com>
2021-02-09 16:13:26 -0500

    testing react-pdf

--- src/Pages/ResumePage.tsx
+++ src/Pages/ResumePage.tsx
@@ -1,15 +1,27 @@
-import { Header } from "../Components/Header/Header";
-import { Footer } from "../Components/Footer/Footer";
-import MiniDrawr from "../Components/MiniDrawer.tsx/MiniDrawr";
-import { ParticlesBackground } from "../Components/ParticlesBackground/ParticlesBackground";
+import { Document, Page } from "react-pdf/dist/esm/entry.webpack";
+import { useState } from "react";
+import { useGlobalStyles } from "../Styles/globalStyles";
+
+const path = "resume.pdf";
 
 export const ResumePage = () => {
-    return (
-      <div>
-        <div style={{ width: '100%', height: '512px', backgroundColor: 'green'}}>
-            Resume Page
-        </div>
-      </div>
-    );
-  };
-  
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+  const globalClasses = useGlobalStyles();
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+    setPageNumber(1);
+    console.log("successfuly loaded");
+  }
+  return (
+    <div className={globalClasses.root}>
+      <Document file={path} onLoadSuccess={onDocumentLoadSuccess}>
+        <Page pageNumber={1} />
+        <Page pageNumber={2} />
+      </Document>
+      <p>
+        Page {pageNumber} of {numPages}
+      </p>
+    </div>
+  );
+};
