0c50e1801b6075de0164b195c698dde68231f557
ArmyaAli <ali.umar.work@gmail.com>
2021-02-09 16:12:44 -0500

    moved to globalStyles

--- src/Components/Footer/footerStyles.ts
+++ src/Components/Footer/footerStyles.ts
@@ -1,19 +1,6 @@
 import { makeStyles } from "@material-ui/core/styles";
 
 export const useFooterStyles = makeStyles((theme) => ({
-    root: {
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'space-between',
-        position: 'relative',
-        height: 'calc(100% - 2.5vh)',
-        maxWidth: 'calc(98% - 57px)',
-        right: 'calc(-1 * (57px/2))',
-        margin: '16px auto',
-        backgroundColor: 'black',
-        padding: '16px',
-        color: 'white'
-    },
     container: {
         position: 'relative',
         display: 'flex',
