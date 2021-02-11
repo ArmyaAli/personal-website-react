a4aae2cf20918b143b0c33fd39119b24bb2896c9
ArmyaAli <ali.umar.work@gmail.com>
2021-02-11 15:59:38 -0500

    changed particle config

---
+++ src/Components/ParticlesBackground/particleConfig.ts
@@ -0,0 +1,75 @@
+export const config ={
+    background: {
+      color: {
+        value: "#cd09e3",
+      },
+    },
+    fpsLimit: 60,
+    interactivity: {
+      detectsOn: "canvas",
+      events: {
+        onClick: {
+          enable: true,
+          mode: "push",
+        },
+        onHover: {
+          enable: true,
+          mode: "repulse",
+        },
+        resize: true,
+      },
+      modes: {
+        bubble: {
+          distance: 400,
+          duration: 2,
+          opacity: 0.8,
+          size: 40,
+        },
+        push: {
+          quantity: 4,
+        },
+        repulse: {
+          distance: 200,
+          duration: 0.4,
+        },
+      },
+    },
+    particles: {
+       color: {
+            value: "#efff00"
+          },
+          shape: {
+           type: "circle",
+            stroke: {
+              width: 0,
+              color: "#000000"
+            },
+      },
+      collisions: {
+        enable: true,
+      },
+      move: {
+        direction: "none",
+        enable: true,
+        outMode: "bounce",
+        random: false,
+        speed: 6,
+        straight: false,
+      },
+      number: {
+        density: {
+          enable: true,
+          value_area: 800,
+        },
+        value: 80,
+      },
+      opacity: {
+        value: 0.5,
+      },
+      size: {
+        random: true,
+        value: 5,
+      },
+    },
+    detectRetina: true,
+  }
