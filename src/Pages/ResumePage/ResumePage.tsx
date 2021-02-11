3ae5d0d0359ff8d348d35765b7cc8b43582c8d7c
ArmyaAli <ali.umar.work@gmail.com>
2021-02-11 16:01:32 -0500

    added resume page template

---
+++ src/Pages/ResumePage/ResumePage.tsx
@@ -0,0 +1,27 @@
+import { Document, Page } from "react-pdf/dist/esm/entry.webpack";
+import { useState } from "react";
+import { useGlobalStyles } from "../../Styles/globalStyles";
+
+const path = "resume.pdf";
+
+export const ResumePage = () => {
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+  const globalClasses = useGlobalStyles();
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+    setPageNumber(1);
+    console.log("successfuly loaded");
+  }
+  return (
+    <div className={globalClasses.root}>
+      <Document file={path} onLoadSuccess={onDocumentLoadSuccess}>
+        <Page pageNumber={1} />
+        <Page pageNumber={2} />
+      </Document>
+      <p>
+        Page {pageNumber} of {numPages}
+      </p>
+    </div>
+  );
+};
