eec0d034bd0d84c21c0a5484a3d3a860e98173da
ArmyaAli <ali.umar.work@gmail.com>
2021-02-11 16:01:24 -0500

    decoupled global for comp specific

--- src/Styles/globalStyles.ts
+++ src/Styles/globalStyles.ts
@@ -6,7 +6,6 @@ export const useGlobalStyles = makeStyles((theme) => ({
         flexDirection: 'column',
         justifyContent: 'space-between',
         position: 'relative',
-        height: 'calc(100% - 2.5vh)',
         maxWidth: 'calc(98% - 57px)',
         right: 'calc(-1 * (57px/2))',
         margin: '16px auto',
